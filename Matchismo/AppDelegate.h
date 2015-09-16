89b2adc89a527d000b06e43cc1f81609369e9c54
Bradford Bennett Jr <bbennettjr@Bradford-Bennett-Jrs-MacBook-Pro.local>
2015-09-15 20:34:41 -0400

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Bradford Bennett Jr on 9/15/15.
+//  Copyright (c) 2015 Rennox, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Bradford Bennett Jr on 9/15/15.
+//  Copyright (c) 2015 Rennox, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
